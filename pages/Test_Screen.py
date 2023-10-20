9d34cc5b7ef9d0678b95de95a4f8f8c71bfd2dd3
ML DOOM <39166535+ML-D00M@users.noreply.github.com>
2023-10-20 09:05:12 +0000

    Test UI tiny changes

--- pages/Test_Screen.py
+++ pages/Test_Screen.py
@@ -1,41 +1,41 @@
 import streamlit as st
 
-def main():
-    st.title("UI Interface")
-    
-    # Central oval: Just representing with a placeholder for now
-    st.markdown("<div style='background-color: grey; border-radius: 50%; width: 200px; height: 300px; margin: auto;'></div>", unsafe_allow_html=True)
-    
-    # Time Display
-    st.sidebar.markdown("### Time: 15:03")
-    
-    # Topics in the sidebar
-    st.sidebar.markdown("### Topics")
-    topic1 = st.sidebar.checkbox("Topic 1: User Research")
-    topic2 = st.sidebar.checkbox("Topic 2: Prototyping")
-    topic3 = st.sidebar.checkbox("Topic 3: Design Solution")
-    
-    # If any topic is selected, show details (you can expand on this!)
-    if topic1:
-        st.write("Details about User Research...")
-    elif topic2:
-        st.write("Details about Prototyping...")
-    elif topic3:
-        st.write("Details about Design Solution...")
-    
-    # Buttons
-    if st.button("Repeat the Question"):
-        st.write("Repeating the question...")
-    
-    col1, col2, col3 = st.columns(3)
-    
-    with col2:
-        if st.button("Leave the Interview"):
-            st.write("Exiting interview...")
-            
-    with col3:
-        if st.button("Skip the Question"):
-            st.write("Skipping to next question...")
+# Sidebar content
+st.sidebar.title("Homepage")
+st.sidebar.text("Behavioral Screen")
+st.sidebar.text("Professional Screen")
+st.sidebar.text("Resume Screen")
+st.sidebar.text("Test Screen")
 
-if __name__ == "__main__":
-    main()
+st.sidebar.subheader("Time: 15:03")
+
+# Topics checkboxes
+st.sidebar.subheader("Topics")
+st.sidebar.checkbox("Topic 1: User Research")
+st.sidebar.checkbox("Topic 2: Prototyping")
+st.sidebar.checkbox("Topic 3: Design Solution")
+
+# Main content
+st.title("UI Interface")
+
+# Placeholder for the large oval shape
+oval_placeholder = st.empty()
+
+# Buttons at the bottom
+st.text("")  # This is just to give some space
+if st.button("Repeat the Question"):
+    st.write("Repeating Question...")
+
+if st.button("Leave the Interview"):
+    st.write("Leaving Interview...")
+
+if st.button("Skip the Question"):
+    st.write("Skipping Question...")
+
+# Drawing the oval using SVG (Scalable Vector Graphics) as a placeholder
+oval_svg = """
+<svg width="300" height="150">
+    <ellipse cx="150" cy="75" rx="140" ry="70" style="fill:#E0E0E0;" />
+</svg>
+"""
+oval_placeholder.markdown(oval_svg, unsafe_allow_html=True)
