9c73c834d1b925a461e86185eebe29806cc0f031
ML DOOM <39166535+ML-D00M@users.noreply.github.com>
2023-10-20 09:34:09 +0000

    test UI

--- pages/Test_Screen.py
+++ pages/Test_Screen.py
@@ -3,30 +3,49 @@ import streamlit as st
 def main():
     st.title("Interview UI")
 
-    # Displaying Time
-    st.write("**Time:** 15.03")
+    # Create two columns for the UI layout
+    col1, col2 = st.columns(2)
 
-    # Displaying Topics
-    topics = [
-        ("Topic 1", "User Research"),
-        ("Topic 2", "Prototyping"),
-        ("Topic 3", "Design Solution")
-    ]
+    # First Column (Left)
+    with col1:
+        # Draw an oval using a placeholder and markdown trick
+        st.markdown(
+            """
+            <div style="background-color: #E0E0E0; height: 150px; border-radius: 75px;"></div>
+            """, 
+            unsafe_allow_html=True
+        )
+        
+        # Buttons under the oval
+        if st.button("CC"):
+            st.write("You pressed 'CC' button.")
+        
+        if st.button("Repeat the question"):
+            st.write("You pressed 'Repeat the question' button.")
 
-    for topic_name, topic_description in topics:
-        st.write(f"**{topic_name}**")
-        st.write(topic_description)
-        st.write("---")  # adding a horizontal line for separation
+    # Second Column (Right)
+    with col2:
+        # Displaying Time
+        st.write("**Time:** 15.03")
 
-    # Displaying Buttons
-    if st.button("Repeat the question"):
-        st.write("You pressed 'Repeat the question' button.")
-    
-    if st.button("Leave the interview"):
-        st.write("You pressed 'Leave the interview' button.")
-    
-    if st.button("Skip the question"):
-        st.write("You pressed 'Skip the question' button.")
+        # Displaying Topics
+        topics = [
+            ("Topic 1", "User Research"),
+            ("Topic 2", "Prototyping"),
+            ("Topic 3", "Design Solution")
+        ]
+
+        for topic_name, topic_description in topics:
+            st.write(f"**{topic_name}**")
+            st.write(topic_description)
+            st.write("---")  # adding a horizontal line for separation
+
+        # Displaying Buttons
+        if st.button("Leave the interview"):
+            st.write("You pressed 'Leave the interview' button.")
+        
+        if st.button("Skip the question"):
+            st.write("You pressed 'Skip the question' button.")
 
 if __name__ == "__main__":
     main()
