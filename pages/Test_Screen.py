dd247fe380ff15b6c4833795c2e6c53819cb0ea9
ML-D00M <ML.DOOM+github@proton.me>
2023-10-28 18:03:36 +0400

    Dynamic page elements testing

--- pages/Test_Screen.py
+++ pages/Test_Screen.py
@@ -6,115 +6,133 @@ def main():
     # Your HTML code
     html_code = """
     <!DOCTYPE html>
-    <html>
+    <html lang="en">
+
     <head>
-        <style>
-            body {
-                font-family: Arial, sans-serif;
-                background-color: #1A1A1A;
-                color: white;
-                margin: 0;
-            }
-            .container {
-                display: flex;
-                justify-content: space-between;
-                padding: 50px;
-                height: 100vh;
-            }
-            .left, .right {
-                flex: 0.45;
-            }
-            .left ul {
-                list-style-type: none;
-                padding: 0;
-            }
-            .left li {
-                background-color: #333;
-                margin-bottom: 10px;
-                padding: 10px;
-                border-radius: 5px;
-            }
-            .right-content {
-                display: flex;
-                flex-direction: column;
-                align-items: center;
-                justify-content: space-between;
-                height: 100%;
-            }
-            .circle {
-                background-color: #333;
-                border-radius: 50%;
-                width: 300px;
-                height: 300px;
-            }
-            .buttons {
-                display: flex;
-                gap: 10px;
-            }
-            button {
-                padding: 10px;
-                background-color: #555;
-                color: white;
-                border: none;
-                border-radius: 5px;
-                cursor: pointer;
-            }
-            .topics {
-                display: flex;
-                flex-direction: column;
-                gap: 10px;
-                align-items: center;
-                padding: 10px;
-            }
-            .topic {
-                padding: 10px;
-                background-color: #333;
-                width: 150px;
-                text-align: center;
-                border-radius: 5px;
-            }
-        </style>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Design Mockup Implementation</title>
+        <link rel="stylesheet" href="styles.css">
     </head>
+
     <body>
         <div class="container">
-            <div class="left">
-                <ul>
-                    <li>Homepage</li>
-                    <li>Behavioral Screen</li>
-                    <li>Professional Screen</li>
-                    <li>Resume Screen</li>
-                    <li>Test Screen</li>
-                </ul>
-            </div>
-            <div class="right">
-                <div class="right-content">
-                    <h1>Interview UI</h1>
-                    <div class="circle"></div>
-                    <div class="topics">
-                        <div class="topic">Topic 1</div>
-                        <div class="topic">Topic 2</div>
-                        <div class="topic">Topic 3</div>
-                    </div>
-                    <div class="buttons">
-                        <button>Leave the interview</button>
-                        <button>Skip the question</button>
-                    </div>
+            <header>
+                <h1>Interview UI</h1>
+                <div class="time-indicator">Time: 15:03</div>
+            </header>
+            <div class="content">
+                <div class="ellipse"></div>
+                <div class="topics">
+                    <div class="topic">User Research</div>
+                    <div class="topic highlighted">Prototyping</div>
+                    <div class="topic">Design Solution</div>
                 </div>
             </div>
+            <footer>
+                <div class="actions">
+                    <button class="repeat-btn">Repeat the question</button>
+                    <button>Leave the interview</button>
+                    <button>Skip the question →</button>
+                </div>
+                <div class="footer-text">Made with Streamlit</div>
+            </footer>
         </div>
     </body>
+
     </html>
+
+
     """
 
-    # # Your CSS code
-    # css_code = """
-    # <style>
-    #     /* Place the CSS code here */
-    # </style>
-    # """
+    # Your CSS code
+    css_code = """
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f6f6f6;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+        }
+
+        .container {
+            background-color: white;
+            border-radius: 10px;
+            padding: 20px;
+            width: 350px;
+            box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+        }
+
+        header {
+            display: flex;
+            justify-content: space-between;
+            margin-bottom: 20px;
+        }
+
+        h1 {
+            margin: 0;
+            color: #333;
+            font-size: 24px;
+        }
+
+        .time-indicator {
+            font-size: 16px;
+        }
+
+        .content {
+            display: flex;
+            align-items: center;
+        }
+
+        .ellipse {
+            background-color: lightgray;
+            width: 150px;
+            height: 150px;
+            border-radius: 50%;
+            margin-right: 30px;
+        }
+
+        .topics .topic {
+            padding: 10px;
+            background-color: white;
+            color: black;
+            margin: 10px 0;
+            border: 1px solid lightgray;
+            border-radius: 5px;
+        }
+
+        .topics .highlighted {
+            background-color: #d0d0d0;
+        }
+
+        footer {
+            margin-top: 20px;
+        }
+
+        .actions {
+            display: flex;
+            justify-content: space-between;
+            margin-bottom: 10px;
+        }
+
+        .repeat-btn:before {
+            content: '⏪';
+            margin-right: 5px;
+        }
+
+        .footer-text {
+            text-align: center;
+            font-size: 12px;
+            color: #888;
+        }
+
+    </style>
+    """
 
     # Display HTML and CSS in Streamlit
-    # st.markdown(css_code, unsafe_allow_html=True)
+    st.markdown(css_code, unsafe_allow_html=True)
     st.markdown(html_code, unsafe_allow_html=True)
 
 if __name__ == "__main__":
